f071953cebb1d20c21b96f281f98d7a2819db156
lanbaoshi123 <635541679@qq.com>
2017-03-16 13:37:35 +0800

    创建demo

---
+++ demo/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import './index.css';
+import { Button } from 'antd-mobile';
+
+ReactDOM.render(
+    <Button> start </Button>,
+  document.getElementById('root')
+);
